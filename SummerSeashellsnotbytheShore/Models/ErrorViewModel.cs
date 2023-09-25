cba4dcf37119d3a5d76ee245c6c06b2b67a7dcc2
Summer - Dell <Summer - Dell@DESKTOP-B9FUR24>
2023-09-24 21:21:19 -0400

    Add project files.

---
+++ SummerSeashellsnotbytheShore/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SummerSeashellsnotbytheShore.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
